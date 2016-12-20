1c85aeca77c1fd166b7e7d8ba99bb3abfa68369e
Georgio Bechara <georgio.bechara@gmail.com>
2016-12-20 12:00:26 +0200

    Update one.java

--- one.java
+++ one.java
@@ -1,37 +1,65 @@
+package com.codiscope.jaks.triggers.java.spring.jdbc.simplejdbctemplate;
 
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
-import java.io.UnsupportedEncodingException; import java.security.InvalidAlgorithmParameterException; import java.security.KeyException;
-import javax.crypto.BadPaddingException; import javax.crypto.Cipher; import javax.crypto.IllegalBlockSizeException; import javax.crypto.NullCipher; import javax.crypto.spec.IvParameterSpec; import javax.crypto.spec.SecretKeySpec;
-public class NullCipherUse {
-public static void main(String[] args) throws Exception {
+import javax.sql.DataSource;
 
-    byte[] pt = "AAAAAAAAAAAAAAAA".getBytes("UTF-8");
+import tests.sources.DatabaseSource;
+import tests.sources.FileSource;
+import tests.sources.PrivateSource;
+import tests.sources.WebSource;
+import tests.sources.WebSourceCookie;
 
-    Cipher expectedCihper = Cipher.getInstance("AES/CBC/NoPadding");
-    Cipher doNothingCihper = new NullCipher();
+/**
+ * The Class JavaSpringJdbcSimplejdbctemplateSqli
+ */
+public class JavaSpringJdbcSimplejdbctemplateSqli {
 
+    private WebSourceCookie webcookie = new WebSourceCookie();
+    private DatabaseSource databasesource = new DatabaseSource();
+    private FileSource filesource = new FileSource();
+    private PrivateSource privatesource = new PrivateSource();
+    private WebSource websource = new WebSource();
 
-    printHex(encryptWithCipher(expectedCihper, pt));
-    printHex(encryptWithCipher(doNothingCihper, pt));
-}
+    private SimpleJdbcTemplate template;
 
-public static byte[] encryptWithCipher(Cipher cipher, byte[] value) throws KeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException,
-        UnsupportedEncodingException {
-
-    //Key generation
-    byte[] passkey = "BBBBBBBBBBBBBBBB".getBytes("UTF-8");
-    Cipher expectedCihper = cipher;
-    SecretKeySpec key = new SecretKeySpec(passkey, "AES");
-    //Setting the key
-    expectedCihper.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(new byte[expectedCihper.getBlockSize()]));
-
-    return cipher.doFinal(value);
-}
-
-private static void printHex(byte[] resultBytes) {
-    for (byte b : resultBytes) {
-        System.out.print(Integer.toHexString(b & 0xFF));
+    public JavaSpringJdbcSimplejdbctemplateSqli(DataSource dataSource) {
+        this.template = new SimpleJdbcTemplate(dataSource);
     }
-    System.out.println();
-}
+
+    public void testQuery() {
+
+        String query = "SELECT COF_NAME FROM COFFEES WHERE ID=" + websource.method2();
+
+        //Test SimpleJdbcOperations query
+        template.query(query, null);
+
+        //Test SimpleJdbcOperations queryForList
+        template.queryForList(query);
+
+        //Test SimpleJdbcOperations queryForInt
+        template.queryForInt(query);
+
+        //Test SimpleJdbcOperations queryForLong
+        template.queryForLong(query);
+
+        //Test SimpleJdbcOperations queryForMap
+        template.queryForMap(query);
+
+        //Test SimpleJdbcOperations queryForObject
+        template.queryForObject(query, String.class);
+    }
+
+    //Test SimpleJdbcOperations Update
+    public void testUpdate() {
+        template.update(databasesource.method1());
+    }
+
+    //Test SimpleJdbcOperations batchUpdate & Execute
+    public void testBatchUpdate() {
+        template.getJdbcOperations().batchUpdate(new String[]{websource.method2()});
+
+        //Test SimpleJdbcOperations execute
+        template.getJdbcOperations().execute(websource.method2());
+    }
 }
